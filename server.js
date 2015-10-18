257ba75f5a09ca6c47374067c641e2147191e8f4
ravi <offlinecode@outlook.com>
2015-10-18 13:38:51 +0530

    5th commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var app = express();
+// Load the http module to create an http server.
+var http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:80/");
